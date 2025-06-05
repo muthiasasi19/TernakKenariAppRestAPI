4426e982c1bac31d4ac129a781def0b57dad1a09
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 00:28:22 +0700

    Buat state untuk kondisi profil buyer lengkap

--- lib/data/model/request/buyer/profile/bloc/profile_buyer_state.dart
+++ lib/data/model/request/buyer/profile/bloc/profile_buyer_state.dart
@@ -4,3 +4,29 @@ part of 'profile_buyer_bloc.dart';
 sealed class ProfileBuyerState {}
 
 final class ProfileBuyerInitial extends ProfileBuyerState {}
+
+final class ProfileBuyerLoading extends ProfileBuyerState {}
+
+final class ProfileBuyerLoaded extends ProfileBuyerState {
+  final BuyerProfileResponseModel profile;
+
+  ProfileBuyerLoaded({required this.profile});
+}
+
+final class ProfilBuyerError extends ProfileBuyerState {
+  final String message;
+
+  ProfilBuyerError({required this.message});
+}
+
+final class ProfileBuyerAdded extends ProfileBuyerState {
+  final BuyerProfileResponseModel profile;
+
+  ProfileBuyerAdded({required this.profile});
+}
+
+final class ProfileBuyerAddedError extends ProfileBuyerState {
+  final String message;
+
+  ProfileBuyerAddedError({required this.profile});
+}
