f3af2caa920ae195e1534fc64a2789d7dfef3745
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-05 20:01:04 +0700

    Buat class untuk kelola status login pengguna

---
+++ lib/presentation/auth/bloc/login_state.dart
@@ -0,0 +1,17 @@
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}
+
+final class LoginSuccess extends LoginState {
+  final AuthResponseModel responseModel;
+
+  LoginSuccess({required this.responseModel});
+}
+
+final class LoginFailure extends LoginState {
+  final String error;
+
+  LoginFailure({required this.error});
+}
