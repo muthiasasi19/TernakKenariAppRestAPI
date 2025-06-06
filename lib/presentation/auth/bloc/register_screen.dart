0c080bba0fc042f6ee4cebae8cce1756c54b385f
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 22:13:05 +0700

    Tambah tampilan dan logika validasi form register

--- lib/presentation/auth/bloc/register_screen.dart
+++ lib/presentation/auth/bloc/register_screen.dart
@@ -39,4 +39,149 @@ class _RegisterScreenState extends State<RegisterScreen> {
     _key.currentState?.dispose();
     super.dispose();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(100),
+                Text(
+                  'DAFTAR AKUN BARU',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SpaceHeight(30),
+                CustomTextField(
+                  validator: 'Username tidak boleh kosong',
+                  controller: namaController,
+                  label: 'Username',
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.person),
+                  ),
+                ),
+                const SpaceHeight(25),
+                CustomTextField(
+                  validator: 'Email tidak boleh kosong',
+                  controller: emailController,
+                  label: 'Email',
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.email),
+                  ),
+                ),
+                const SpaceHeight(25),
+                Row(
+                  children: [
+                    Expanded(
+                      child: CustomTextField(
+                        validator: 'Password tidak boleh kosong',
+                        controller: passwordController,
+                        label: 'Password',
+                        obscureText: !isShowPassword,
+                        prefixIcon: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.lock),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isShowPassword = !isShowPassword;
+                            });
+                          },
+                          icon: Icon(
+                            isShowPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: AppColors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SpaceHeight(50),
+                BlocConsumer<RegisterBloc, RegisterState>(
+                  listener: (context, state) {
+                    if (state is RegisterSuccess) {
+                      context.pushAndRemoveUntil(
+                        const LoginScreen(),
+                        (route) => false,
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.message),
+                          backgroundColor: AppColors.primary,
+                        ),
+                      );
+                    } else if (state is RegisterFailure) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(state.error),
+                          backgroundColor: AppColors.red,
+                        ),
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    return Button.filled(
+                      onPressed:
+                          state is RegisterLoading
+                              ? null
+                              : () {
+                                if (_key.currentState!.validate()) {
+                                  final request = RegisterRequestModel(
+                                    username: namaController.text,
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                                  context.read<RegisterBloc>().add(
+                                    RegisterRequested(requestModel: request),
+                                  );
+                                }
+                              },
+                      label: state is RegisterLoading ? 'Memuat...' : 'Daftar',
+                    );
+                  },
+                ),
+                const SpaceHeight(20),
+                Text.rich(
+                  TextSpan(
+                    text: 'Sudah memiliki akun? Silahkan ',
+                    style: TextStyle(
+                      color: AppColors.grey,
+                      fontSize: MediaQuery.of(context).size.width * 0.03,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Login disini',
+                        style: TextStyle(color: AppColors.primary),
+                        recognizer:
+                            TapGestureRecognizer()
+                              ..onTap = () {
+                                context.pushAndRemoveUntil(
+                                  const LoginScreen(),
+                                  (route) => false,
+                                );
+                              },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
