73b70384f66d14f156f6789a52a871c619a3dbf8
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 10:35:47 +0700

    Tambah logika BLoC untuk tambah dan ambil profil

--- lib/data/model/request/buyer/profile/bloc/profile_buyer_bloc.dart
+++ lib/data/model/request/buyer/profile/bloc/profile_buyer_bloc.dart
@@ -5,9 +5,37 @@ part 'profile_buyer_event.dart';
 part 'profile_buyer_state.dart';
 
 class ProfileBuyerBloc extends Bloc<ProfileBuyerEvent, ProfileBuyerState> {
-  ProfileBuyerBloc() : super(ProfileBuyerInitial()) {
-    on<ProfileBuyerEvent>((event, emit) {
-      // TODO: implement event handler
+  final ProfileBuyerRepository profileBuyerRepository;
+  ProfileBuyerBloc({required this.profileBuyerRepository})
+    : super(ProfileBuyerInitial()) {
+    on<AddProfileBuyerEvent>(_addProfileBuyer);
+    on<GetProfileBuyerEvent>(_getProfileBuyer);
+  }
+
+  Future<void> _addProfileBuyer(
+    AddProfileBuyerEvent event,
+    Emitter<ProfileBuyerState> emit,
+  ) async {
+    emit(ProfileBuyerLoading());
+    final result = await profileBuyerRepository.addProfileBuyer(
+      event.requestModel,
+    );
+    result.fold((error) => emit(ProfileBuyerAddError(message: error)), (
+      profile,
+    ) {
+      emit(ProfileBuyerAdded(profile: profile));
     });
+  }
+
+  Future<void> _getProfileBuyer(
+    GetProfileBuyerEvent event,
+    Emitter<ProfileBuyerState> emit,
+  ) async {
+    emit(ProfileBuyerLoading());
+    final result = await profileBuyerRepository.getProfileBuyer();
+    result.fold(
+      (error) => emit(ProfileBuyerError(message: error)),
+      (profile) => emit(ProfileBuyerLoaded(profile: profile)),
+    );
   }
 }
