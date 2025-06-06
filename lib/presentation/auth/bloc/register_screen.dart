202ef651c9fc38e74c229059afd46230ef3ff2a2
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 21:47:21 +0700

    Buat logika dasar register dengan controller dan form

--- lib/presentation/auth/bloc/register_screen.dart
+++ lib/presentation/auth/bloc/register_screen.dart
@@ -1,1 +1,42 @@
+import 'package:canary/core/components/custom_text_field.dart';
+import 'package:canary/core/components/spaces.dart';
+import 'package:canary/core/core.dart';
+import 'package:canary/data/model/request/auth/register_request_model.dart';
+import 'package:canary/presentation/auth/bloc/login_screen.dart';
+import 'package:canary/presentation/auth/bloc/register/register_bloc.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  late final TextEditingController namaController;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final GlobalKey<FormState> _key;
+  bool isShowPassword = false;
+
+  @override
+  void initState() {
+    namaController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    _key = GlobalKey<FormState>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    namaController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    _key.currentState?.dispose();
+    super.dispose();
+  }
+}
