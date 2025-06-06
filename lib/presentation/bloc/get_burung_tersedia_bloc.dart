7b5ff9c681cae9afbf36a222e9cfc5891396df35
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 17:35:50 +0700

    Tambahkan BLoC untuk mengambil daftar burung yang tersedia

--- lib/presentation/bloc/get_burung_tersedia_bloc.dart
+++ lib/presentation/bloc/get_burung_tersedia_bloc.dart
@@ -1,13 +1,30 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:canary/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canary/data/repository/get_all_burung_tersedia_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'get_burung_tersedia_event.dart';
 part 'get_burung_tersedia_state.dart';
 
-class GetBurungTersediaBloc extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
-  GetBurungTersediaBloc() : super(GetBurungTersediaInitial()) {
-    on<GetBurungTersediaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class GetBurungTersediaBloc
+    extends Bloc<GetBurungTersediaEvent, GetBurungTersediaState> {
+  final GetAllBurungTersediaRepository getAllBurungTersediaRepository;
+
+  GetBurungTersediaBloc(this.getAllBurungTersediaRepository)
+    : super(GetBurungTersediaInitial()) {
+    on<GetAllBurungTersediaEvent>(_getAllBurungTersedia);
+  }
+
+  Future<void> _getAllBurungTersedia(
+    GetAllBurungTersediaEvent event,
+    Emitter<GetBurungTersediaState> emit,
+  ) async {
+    emit(GetBurungTersediaLoading());
+    final result = await getAllBurungTersediaRepository.getAllBurungTersedia();
+    result.fold(
+      (error) => emit(GetBurungTersediaError(message: error)),
+      (burungTersedia) =>
+          emit(GetBurungTersediaLoaded(burungTersedia: burungTersedia)),
+    );
   }
 }
