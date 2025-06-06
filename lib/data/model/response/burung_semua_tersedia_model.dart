eddaf62a75d234609eaf3da96b0dcfd5e10dfff8
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 15:43:13 +0700

    Tambah model data burung dan konversi JSON

---
+++ lib/data/model/response/burung_semua_tersedia_model.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+class BurungSemuaTersediaModel {
+  final String message;
+  final int statusCode;
+  final List<DataBurungTersedia> data;
+
+  BurungSemuaTersediaModel({
+    required this.message,
+    required this.statusCode,
+    required this.data,
+  });
+
+  BurungSemuaTersediaModel copyWith({
+    String? message,
+    int? statusCode,
+    List<DataBurungTersedia>? data,
+  }) => BurungSemuaTersediaModel(
+    message: message ?? this.message,
+    statusCode: statusCode ?? this.statusCode,
+    data: data ?? this.data,
+  );
+
+  factory BurungSemuaTersediaModel.fromRawJson(String str) =>
+      BurungSemuaTersediaModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory BurungSemuaTersediaModel.fromJson(
+    Map<String, dynamic> json,
+  ) => BurungSemuaTersediaModel(
+    message: json["message"],
+    statusCode: json["status_code"],
+    // data: List<DataBurungTersedia>.from(json["data"].map((x) => DataBurungTersedia.fromJson(x))),
+    data:
+        json["data"] == null
+            ? []
+            : List<DataBurungTersedia>.from(
+              json["data"].map((x) => DataBurungTersedia.fromJson(x)),
+            ),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "message": message,
+    "status_code": statusCode,
+    "data": List<dynamic>.from(data.map((x) => x.toJson())),
+  };
+}
