adb431c2a0ca93803c2221b4dc8f62aa7d39cf6e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 13:36:10 +0700

    Implement Bloc untuk proses registrasi pengguna baru

--- lib/presentation/auth/register/register_bloc.dart
+++ lib/presentation/auth/register/register_bloc.dart
@@ -1,1 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:canary/data/model/request/auth/register_request_model.dart';
+import 'package:canary/data/repository/auth_repository.dart';
+import 'package:canary/presentation/auth/register/register_state.dart';
+import 'package:meta/meta.dart';
 
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  final AuthRepository authRepository;
+  RegisterBloc({required this.authRepository}) : super(RegisterInitial()) {
+    on<RegisterRequested>(_onRegisterRequested);
+  }
+
+  Future<void> _onRegisterRequested(
+    RegisterRequested event,
+    Emitter<RegisterState> emit,
+  ) async {
+    emit(RegisterLoading());
+
+    final result = await authRepository.register(event.requestModel);
+
+    result.fold(
+      (l) => emit(RegisterFailure(error: l)),
+      (r) => emit(RegisterSuccess(message: r)),
+    );
+  }
+}
