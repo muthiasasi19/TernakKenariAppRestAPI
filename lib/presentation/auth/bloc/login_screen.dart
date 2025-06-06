dfc1dd2ae72088396357d49afc8b9a34247bf784
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-06-06 14:37:35 +0700

    Buat tampilan login dengan validasi form

--- lib/presentation/auth/bloc/login_screen.dart
+++ lib/presentation/auth/bloc/login_screen.dart
@@ -1,1 +1,39 @@
 
+import 'package:canary/core/components/components.dart';
+import 'package:canary/core/components/spaces.dart';
+import 'package:canary/core/core.dart';
+import 'package:canary/data/model/request/auth/login_request_model.dart';
+import 'package:canary/presentation/auth/bloc/login/login_bloc.dart';
+import 'package:canary/presentation/auth/bloc/login/login_state.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final GlobalKey<FormState> _key;
+  bool isShowPassword = false;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    _key = GlobalKey<FormState>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    _key.currentState?.dispose();
+    super.dispose();
+  }
